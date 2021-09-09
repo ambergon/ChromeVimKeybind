c051607ccedb47fe5f10cd79536ad55c25f09f7c
ambergon <sc.foxgon@gmail.com>
2021-09-09 09:33:41 +0900

    [update]
    実装項目
    
    Shift - J コンテンツを選択する：下
    Shift - K コンテンツを選択する：上
    
    Enter コンテンツが選択されている場合そのリンクを開く

--- content.js
+++ content.js
@@ -23,7 +23,20 @@ function register(){
 }
 
 
+//一つ前の入力を保存
 var before_key;
+
+//one of list
+var choose_int = -1;
+//緑
+var choose_color = '#008000';
+//var choose_color = '#ea5532';
+
+//google class名
+var chrome_class_name = 'yuRUbf';
+//配列化
+div_list = document.getElementsByClassName( chrome_class_name );
+
 function someMethod(e) {
 
     if(focus_status == false){
@@ -39,6 +52,56 @@ function someMethod(e) {
                 case 80:
                     history.back();
                     break;
+                 
+                //J
+                case 74:
+                    if( -1 <= choose_int && choose_int < div_list.length ){
+                        if(choose_int != -1){
+                        //カラーの初期化
+                        before_block = div_list[ choose_int ];
+                        before_block.style.backgroundColor = null;
+                        }
+
+                        choose_int++;
+
+                        if(choose_int != div_list.length ){
+                            //選択した項目の背景色を変更
+                            block = div_list[ choose_int ];
+                            block.style.backgroundColor = choose_color;
+
+                            //console.log( block );
+                        }
+                        //console.log( choose_int );
+                    }
+                    break;
+                    
+                //K
+                case 75:
+
+                    if( 0 <= choose_int && choose_int <= div_list.length ){
+                        if(choose_int != div_list.length ){
+                            //カラーの初期化
+                            before_block = div_list[ choose_int ];
+                            before_block.style.backgroundColor = null;
+                        }
+
+                        choose_int--;
+
+                        if( 0 <= choose_int ){
+                            //選択した項目の背景色を変更
+                            block = div_list[ choose_int ];
+                            block.style.backgroundColor = choose_color;
+
+                            //console.log( block );
+                        }
+                        //console.log( choose_int );
+                    }
+                    break;
+                case 71:
+                    var element = document.documentElement;
+                    var bottom = element.scrollHeight - element.clientHeight;
+                    window.scroll({ top : bottom , behavior : 'smooth' });
+                    break;
             }
         }
 
@@ -53,60 +116,55 @@ function someMethod(e) {
 
                 //J + down browser
                 case 74:
-                    window.scrollBy(0, 30 );
+                    window.scrollBy(0, 50 );
                     break;
 
                 //K + up   browser
                 case 75:
-                    window.scrollBy(0, -30 );
+                    window.scrollBy(0, -50 );
                     break;
 
-                //E = 新しいタブを開く
-                case 69:
-                    window.open('https://www.google.com');
-                    break;
-
-                //:Q = コマンドで開いたタブを閉じる
-                case 81:
-                    if(before_key == 186){
-                        window.close('');
+                //GG + go to top
+                case 71:
+                    if( before_key == 71 ){
+                        window.scroll({ top: 0 , behavior: 'smooth' });
                     }
                     break;
 
-                //test F
+
+
+
+                //enter
+                case 13:
+                    if(choose_int != -1 && choose_int != div_list.length ){
+                        var text = div_list[ choose_int ];
+                        var style = div_list[ choose_int ].style;
+                        var raw_text = div_list[ choose_int ].innerHTML;
+
+                        var result = raw_text.match(/href=\".*?\"/gi);
+
+                        //console.log( text );
+                        //console.log( style );
+                        //console.log( raw_text );
+                        //console.log( result[0] );
+
+                        var link = result[0].slice( 5 ).replace(/"/g , '');
+                        console.log( link );
+                        window.open( link , '_self');
+
+                    }
+                    break;
+
+                //F
                 case 70:
-                    ggg = document.getElementsByClassName('fi');
-                    var g = ggg[0];
-                    //console.log( 'debag');
-                    window.alert( g );
-                    //var ggg = document.getElementsByClassName('gLFyf');
-                    //window.alert("aaa:  " + ggg[] );
-                    //$('gLFyf').first.focus();
-                    //var url_a_tag = document.getElementsByClassName('r');
-                    //var title = document.getElementsByClassName('ellip');
-                    //var content = document.getElementsByClassName('st');
-                    //var data = '';
-
-                    ////タイトルタグだけあってURLがない場合にエラーが出るのでurl_a_tag.lengthとしてある
-                    //for(var i = 0 ;  i < url_a_tag.length ; i++){
-                    //    var a_tag = url_a_tag[i].getElementsByTagName('a');
-
-                    //    data = data + title[i].innerText+'\n';
-                    //    data = data + a_tag[0].href+'\n';
-                    //    data = data + content[i].innerText+'\n';
-                    //    data = data + '\n';
-                    //}
-                    //console.log(data);
-                    //console.log( 'debag' );
                     break;
 
 
-                default:
-                    //window.alert('set key' + e.keyCode + ' : and ctrl? = ' + e.ctrlKey);
+                //default:
             }
         }
-        //window.alert('before = '+before_key);
-        before_key = e.keyCode
+        //keyの保存
+        before_key = e.keyCode;
     }
 }
 
